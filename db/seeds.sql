01ba35f65d7f36d95a3e9cafacaf5e7413714755
Kelsey Elmer <kguitar6@Ks-MacBook-Air.local>
2017-08-15 13:35:35 -0600

    need to fix post and put

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger, devoured) VALUES ('Double Bacon Cheeseburger', true);
+INSERT INTO burgers (burger, devoured) VALUES ('BBQ Cowboy Burger', true);
+INSERT INTO burgers (burger, devoured) VALUES ('Black and Blue Burger', true);
+
+INSERT INTO burgers (burger, devoured) VALUES ('Ahi Tuna Burger', false);
+INSERT INTO burgers (burger, devoured) VALUES ('Veggie Burger', false);
